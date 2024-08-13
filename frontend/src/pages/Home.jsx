350e4dd1332c737d133733b07fa8fc0dc8a8ed40
brian <muriikibrian9@gmail.com>
2024-08-13 18:31:55 +0300

    created assets

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,18 +1,23 @@
 import vite from "/vite.svg";
+// import landingimage1 from "//Assets/Landing-image-1.svg";
 
 function Home() {
   return (
-  <div className="px-4 my-8">
-<<<<<<< HEAD
-=======
-    {/* hero section */}
->>>>>>> aafbc69359b8785f7008e0bb622463fc4c317764
-    <div className="hero">
-      <div className="headline"></div>
-      <div className="illustration"></div>
+    <div className="px-4 my-8">
+      {/* hero section */}
+      <div className="hero">
+        <div className="headline">
+          <h1 className="">
+            Embrace your beauty journey! Discover a world of pampering and
+            self-care at your fingertips
+          </h1>
+        </div>
+        <div className="illustration">
+          <img src={landingimage1} alt="" />
+        </div>
+      </div>
     </div>
-
-  </div>
-)}
+  );
+}
 
 export default Home;
