e332bff4627acc33676dcb6df18283425ed98851
brian <muriikibrian9@gmail.com>
2024-08-13 12:17:32 +0300

    home page initial commit

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,9 +2,13 @@ import vite from "/vite.svg";
 
 function Home() {
   return (
-  <div className="animate-pulse my-96 flex flex-col ">
-    <h1 className="font-semibold">Landing Page</h1>
-    <img src={vite} alt="" className=""  />
+  <div className="px-4 my-8">
+    {/* hero section */}
+    <div className="hero">
+      <div className="headline"></div>
+      <div className="illustration"></div>
+    </div>
+
   </div>
 )}
 
