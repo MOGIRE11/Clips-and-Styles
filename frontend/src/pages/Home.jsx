a616146bcdc1bc15e0a401e6865379111cffba80
brian <muriikibrian9@gmail.com>
2024-08-29 12:02:51 +0300

    Resolved React Router queries

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,9 @@
-
+import Navbar from "../components/Navbar";
 import landingimage1 from "./Assets/Landingimage1.svg";
 export default function Home() {
   return (
     <div className="px-4 my-8">
-      {/* hero section */}
+      {/*hero section */}
       <div className="hero flex gap-10">
         <div
           className="headline w-[50%] flex flex-col
@@ -15,7 +15,11 @@ export default function Home() {
           </h1>
         </div>
         <div className="illustration border">
-          <img src={landingimage1} alt="" className="rounded-xl w-[676px] h-[464px]" />
+          <img
+            src={landingimage1}
+            alt=""
+            className="rounded-xl w-[676px] h-[464px]"
+          />
         </div>
       </div>
     </div>
